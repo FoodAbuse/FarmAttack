d55736017b87aac494253e809633a5e42363d25b
RoodMoose1 <117737924+RoodMoose1@users.noreply.github.com>
2024-02-03 16:24:17 +0800

    forgot
    i forgor what i added

--- Project Folder/Farm Attack 2 Attack of the Sigmas/Assets/gumballEnemyBehaviour.cs
+++ Project Folder/Farm Attack 2 Attack of the Sigmas/Assets/gumballEnemyBehaviour.cs
@@ -10,27 +10,31 @@ public class gumballEnemyBehaviour : MonoBehaviour
 
     public GameObject GumballProjectile;
 
+    public float rotationSpeed = 5.0f; // Adjust the speed as needed
     float timer;
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform;  
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Body.LookAt(player);
+        Vector3 playerPosition = new Vector3(player.position.x, Body.position.y, player.position.z);
+
+        // Smoothly rotate towards the player with the specified rotation speed
+        Quaternion targetRotation = Quaternion.LookRotation(playerPosition - Body.position);
+        Body.rotation = Quaternion.Slerp(Body.rotation, targetRotation, rotationSpeed * Time.deltaTime);
 
         timer += Time.deltaTime;
-        if(timer > .75f)
+        if (timer > 0.75f)
         {
             GameObject newGumball = Instantiate(GumballProjectile, shootPosition.position, shootPosition.rotation);
             newGumball.GetComponent<Rigidbody>().AddForce(newGumball.transform.forward * 2500);
             Destroy(newGumball, 5);
             timer = 0;
         }
-
     }
 }
